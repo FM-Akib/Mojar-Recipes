c437fca3b59cd17197d1add393fa3cecad5f46d2
FM-Akib <muntasirakib08@gmail.com>
2024-01-28 13:06:57 +0600

    loader added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,18 @@
 // import React from 'react';
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
-
+import'./Home.css';
 const Home = () => {
+    const navigation =useNavigation();
     return (
         <div>
            <Header/>
-           
+           <div className="flex items-center justify-center">
+            {
+                navigation.state==='loading'?<span className="loading loading-dots loading-lg loaderHeight"></span>:''
+            }
+           </div>
            <Outlet/>
         </div>
     );
